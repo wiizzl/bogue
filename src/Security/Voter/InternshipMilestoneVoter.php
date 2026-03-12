592e7ad403aeb574372c652239dc4d4338e447e2
Pierre H. <git@houlliere.com>
2026-03-12 20:17:34 +0100

    feat: add InternshipVoter for managing internship access control

--- src/Security/Voter/InternshipMilestoneVoter.php
+++ src/Security/Voter/InternshipMilestoneVoter.php
@@ -7,6 +7,7 @@ use App\Entity\User;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Authorization\Voter\Vote;
 
 class InternshipMilestoneVoter extends Voter
 {
@@ -21,7 +22,7 @@ class InternshipMilestoneVoter extends Voter
         return in_array($attribute, [self::EDIT]) && $subject instanceof InternshipMilestone;
     }
 
-    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token, ?Vote $vote = null): bool
     {
         $user = $token->getUser();
 
