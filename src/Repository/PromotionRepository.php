ccf699f5b496c7a448e800f6d5930f917fd27fef
Pierre Houllière <git@houlliere.com>
2026-03-16 10:48:08 +0100

    refactor: remove commented-out methods in PromotionRepository for cleaner code

--- src/Repository/PromotionRepository.php
+++ src/Repository/PromotionRepository.php
@@ -28,29 +28,4 @@ class PromotionRepository extends ServiceEntityRepository
             ->getQuery()
             ->getResult();
     }
-
-    //    /**
-    //     * @return Promotion[] Returns an array of Promotion objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Promotion
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
