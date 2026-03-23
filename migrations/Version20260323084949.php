fc41fa032c18d5213cc9719b34d5d4688809c6a2
Pierre Houllière <git@houlliere.com>
2026-03-23 09:51:32 +0100

    fix: bump migration

--- migrations/Version20260323084949.php
+++ migrations/Version20260323084949.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260322095854 extends AbstractMigration
+final class Version20260323084949 extends AbstractMigration
 {
     public function getDescription(): string
     {
